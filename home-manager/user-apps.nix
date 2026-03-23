819734d4abdf3fa0b49c0e6eeed40f63c9f93233
Trace Ohrt <teohrt18@gmail.com>
2026-03-22 22:30:45 -0400

    move eza to system

--- home-manager/user-apps.nix
+++ home-manager/user-apps.nix
@@ -4,7 +4,6 @@
     neofetch
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
-    eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
     which
     tree
--- nixos/system-apps.nix
+++ nixos/system-apps.nix
@@ -3,5 +3,6 @@
   environment.systemPackages = with pkgs; [
     git
     neovim
+    eza # A modern replacement for ‘ls’
   ];
 }
