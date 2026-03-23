c93bff6ea6b7e02f3df9fcc08f65105196e7ba43
Trace Ohrt <teohrt18@gmail.com>
2026-03-22 21:08:36 -0400

    home manager intro

--- flake.nix
+++ flake.nix
@@ -1,16 +1,38 @@
 {
-  description = "A simple NixOS flake";
+  description = "NixOS configuration";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      my-nixos = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.trace = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "trace";
+  home.homeDirectory = "/home/trace";
+
+  # Import files from the current configuration directory into the Nix store,
+  # and create symbolic links pointing to those store files in the Home directory.
+
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # Import the scripts directory into the Nix store,
+  # and recursively generate symbolic links in the Home directory pointing to the files in the store.
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    neofetch
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+    which
+    tree
+    gawk
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Trace Ohrt";
+    userEmail = "teohrt18@gmail.com";
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+}
