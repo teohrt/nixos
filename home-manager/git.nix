4290c02f99b3155bba15ae770d01623396726bd5
Trace Ohrt <teohrt18@gmail.com>
2026-03-24 22:30:24 -0400

    fix git config

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,11 +1,9 @@
 { ... }: {
   programs.git = {
     enable = true;
-    settings = {
-      user = {
-        name = "Trace Ohrt";
-        email = "teohrt18@gmail.com";
-      };
+    userName = "Trace Ohrt";
+    userEmail = "teohrt18@gmail.com";
+    extraConfig = {
       pull.rebase = false;
       core.editor = "nvim";
     };
