2595e82d67ac896976ee5de6c7dc9974d21104f6
Trace Ohrt <teohrt18@gmail.com>
2026-03-26 20:15:34 -0400

    another shot

--- home-manager/rofi-app-launcher.nix
+++ home-manager/rofi-app-launcher.nix
@@ -2,8 +2,11 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi;
+    plugins = [
+      (pkgs.rofi-calc.override { rofi-unwrapped = pkgs.rofi-unwrapped; })
+    ];
     extraConfig = {
-      modi = "drun,window,calc:${pkgs.rofi-calc}/lib/rofi/calc.so";
+      modi = "drun,window,calc";
       show-icons = true;
       drun-display-format = "{icon} {name}";
       disable-history = false;
