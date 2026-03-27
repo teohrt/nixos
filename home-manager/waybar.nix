df24f714871674d4544c4e775e00702bf64296f6
Trace Ohrt <teohrt18@gmail.com>
2026-03-27 18:01:50 -0400

    another

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -134,7 +134,7 @@ in
       #custom-launcher {
         background: rgba(10, 10, 15, 0.85);
         border-radius: 12px;
-        padding: 0 7px;
+        padding: 0 9px 0 5px;
         font-size: 16px;
       }
 
@@ -178,7 +178,7 @@ in
       #network {
         background: rgba(10, 10, 15, 0.85);
         border-radius: 12px;
-        padding: 0 7px;
+        padding: 0 9px 0 5px;
       }
 
       #battery,
