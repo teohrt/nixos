fa41a3eaa3170900c1e34245a6f7ebb77904d6bc
Trace Ohrt <teohrt18@gmail.com>
2026-03-26 20:53:14 -0400

    hail marry

--- home-manager/user-apps.nix
+++ home-manager/user-apps.nix
@@ -1,6 +1,9 @@
 # Packages that should be installed to the user profile.
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    # rofi-calc backend
+    qalculate-gtk
+
     # cli utilities
     ripgrep
     jq
