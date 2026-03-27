50d405630700d4b12f59f785e227332fca9344d1
Trace Ohrt <teohrt18@gmail.com>
2026-03-26 20:07:49 -0400

    another attempt

--- home-manager/rofi-app-launcher.nix
+++ home-manager/rofi-app-launcher.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }: {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi.override {
-      plugins = [ pkgs.rofi-calc ];
-    };
+    package = pkgs.rofi;
     extraConfig = {
-      modi = "drun,window,calc";
+      modi = "drun,window,calc:${pkgs.rofi-calc}/lib/rofi/calc.so";
       show-icons = true;
       drun-display-format = "{icon} {name}";
       disable-history = false;
