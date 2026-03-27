2c73e4bd1977a0f8c28310fa12e9c67cee4fbd93
Trace Ohrt <teohrt18@gmail.com>
2026-03-26 20:41:13 -0400

    another

--- home-manager/rofi-app-launcher.nix
+++ home-manager/rofi-app-launcher.nix
@@ -7,6 +7,7 @@
     ];
     extraConfig = {
       modi = "drun,window,calc";
+      plugin-path = "${pkgs.rofi-calc}/lib/rofi";
       show-icons = true;
       drun-display-format = "{icon} {name}";
       disable-history = false;
