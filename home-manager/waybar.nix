b347da896dd34cc31175782d9c18ff8c8abc21f6
Trace Ohrt <teohrt18@gmail.com>
2026-03-27 18:15:08 -0400

    shift

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -134,7 +134,7 @@ in
       #custom-launcher {
         background: rgba(10, 10, 15, 0.85);
         border-radius: 12px;
-        padding: 0 18px 0 10px;
+        padding: 0 13px 0 15px;
         font-size: 16px;
       }
 
@@ -178,7 +178,7 @@ in
       #network {
         background: rgba(10, 10, 15, 0.85);
         border-radius: 12px;
-        padding: 0 18px 0 10px;
+        padding: 0 13px 0 15px;
       }
 
       #battery,
