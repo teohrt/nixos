eae63c36806865b3bc03f37d8ecc5c807b8df164
Trace Ohrt <teohrt18@gmail.com>
2026-03-28 13:58:05 -0400

    test

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -3,7 +3,7 @@
 let
   powerMenu = pkgs.writeShellScript "waybar-power-menu" ''
     CHOICE=$(printf "Shutdown\nRestart\nLock\nSuspend\nLog Out" \
-      | ${pkgs-walker.walker}/bin/walker -n --dmenu -t power -N -H)
+      | ${pkgs-walker.walker}/bin/walker --dmenu -N -H)
     case "$CHOICE" in
       Shutdown) systemctl poweroff ;;
       Restart)  systemctl reboot ;;
