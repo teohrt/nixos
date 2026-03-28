c277b9812d90d2fe82fc600fd054740144ad26b4
Trace Ohrt <teohrt18@gmail.com>
2026-03-28 17:40:19 -0400

    thicker focus

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -38,7 +38,7 @@
       general = {
         gaps_in = 5;   # gap between tiled windows
         gaps_out = 10; # gap between windows and screen edge
-        border_size = 1;
+        border_size = 2;
         "col.active_border" = "rgba(ffffff40)";   # subtle white border on focused window
         "col.inactive_border" = "rgba(00000000)"; # no border on unfocused windows
         layout = "dwindle"; # binary space partitioning layout
