b67b7192a6461a6f0b962d8cdc8090c73d8772d4
Trace Ohrt <teohrt18@gmail.com>
2026-03-28 13:48:05 -0400

    smaller power menu

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -3,7 +3,7 @@
 let
   powerMenu = pkgs.writeShellScript "waybar-power-menu" ''
     CHOICE=$(printf "Shutdown\nRestart\nLock\nSuspend\nLog Out" \
-      | ${pkgs-walker.walker}/bin/walker --dmenu -p "Power" --width 200 -N -H)
+      | ${pkgs-walker.walker}/bin/walker --dmenu -p "Search..." --width 150 -N -H)
     case "$CHOICE" in
       Shutdown) systemctl poweroff ;;
       Restart)  systemctl reboot ;;
