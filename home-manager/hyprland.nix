69dc8ffc61bd58eb040ed6304b4c40aae65ca3f0
Trace Ohrt <teohrt18@gmail.com>
2026-03-28 21:42:35 -0400

    allow 10 workspaces

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -120,6 +120,11 @@
         "$mod, 3, workspace, 3"
         "$mod, 4, workspace, 4"
         "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
 
         # move active window to workspace
         "$mod SHIFT, 1, movetoworkspace, 1"
@@ -127,6 +132,11 @@
         "$mod SHIFT, 3, movetoworkspace, 3"
         "$mod SHIFT, 4, movetoworkspace, 4"
         "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
 
         # mute toggles (swayosd shows the OSD popup)
         ", XF86AudioMute,    exec, swayosd-client --output-volume mute-toggle"
