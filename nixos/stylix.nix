9a6c6db09de33f8c7a38279a1ec11b634e321f49
Trace Ohrt <teohrt18@gmail.com>
2026-03-31 23:46:56 -0400

    stylix opacity

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -4,5 +4,12 @@
     image = ../assets/wallpaper.png;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+
+    opacity = {
+      terminal     = 0.85;
+      applications = 0.90;
+      popups       = 0.85;
+      desktop      = 1.0;
+    };
   };
 }
