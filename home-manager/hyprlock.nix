aa56d3e6992701869e881392e8c3745a7bc861d4
Trace Ohrt <teohrt18@gmail.com>
2026-03-31 23:29:35 -0400

    hyperlock input field

--- home-manager/hyprlock.nix
+++ home-manager/hyprlock.nix
@@ -9,27 +9,6 @@
         grace = 0;
       };
 
-      input-field = [
-        {
-          monitor = "";
-          size = "300, 50";
-          position = "0, -80";
-          halign = "center";
-          valign = "center";
-          outline_thickness = 2;
-          dots_size = 0.2;
-          dots_spacing = 0.15;
-          dots_center = true;
-          outer_color = "rgb(136, 192, 208)";   # Nord blue #88C0D0
-          inner_color = "rgb(59, 66, 82)";       # Nord dark #3B4252
-          font_color = "rgb(236, 239, 244)";     # Nord light #ECEFF4
-          fade_on_empty = true;
-          placeholder_text = "";
-          check_color = "rgb(163, 190, 140)";    # Nord green
-          fail_color = "rgb(191, 97, 106)";      # Nord red
-          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-        }
-      ];
 
       label = [
         # Clock
