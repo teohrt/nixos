b661b9fed9a885d8a8eca88aa6dc09e5c7a34f43
Trace Ohrt <teohrt18@gmail.com>
2026-03-31 23:51:48 -0400

    cursor

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -1,9 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  bibata = pkgs.bibata-cursors;
+in {
   stylix = {
     enable = true;
     image = ../assets/wallpaper.png;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+
+    cursor = {
+      package = bibata;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
 
     opacity = {
       terminal     = 0.6;
