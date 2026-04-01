ccdcac97b035893f076167bc31b36eea7c810553
Trace Ohrt <teohrt18@gmail.com>
2026-03-31 23:44:18 -0400

    manual theme - wallpaper sampling seems broken

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -1,7 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   stylix = {
     enable = true;
     image = ../assets/wallpaper.png;
     polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
   };
 }
