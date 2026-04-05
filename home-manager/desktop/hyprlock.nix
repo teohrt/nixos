1dc32ad3130728cb55ede02ad83ec6703829b80a
Trace Ohrt <teohrt18@gmail.com>
2026-04-05 18:10:11 -0400

    remove all of my lockscreen overrides

--- home-manager/desktop/hyprlock.nix
+++ home-manager/desktop/hyprlock.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ ... }:
 
 {
   programs.hyprlock = {
@@ -9,44 +9,6 @@
         grace = 0;
       };
 
-      background = lib.mkForce [{
-        monitor = "";
-        color = "rgb(0, 0, 0)";
-      }];
-
-      input-field = [{
-        monitor = "";
-        size = "400, 50";
-        position = "0, -100";
-        halign = "center";
-        valign = "center";
-      }];
-
-
-      label = [
-        # Clock
-        {
-          monitor = "";
-          text = ''cmd[update:1000] echo "$(date +"%H:%M")"'';
-          color = "rgba(236, 239, 244, 1.0)";
-          font_size = 90;
-          font_family = "JetBrainsMono Nerd Font";
-          position = "0, 160";
-          halign = "center";
-          valign = "center";
-        }
-        # Date
-        {
-          monitor = "";
-          text = ''cmd[update:1000] echo "$(date +"%A, %B %d")"'';
-          color = "rgba(236, 239, 244, 0.8)";
-          font_size = 18;
-          font_family = "JetBrainsMono Nerd Font";
-          position = "0, 60";
-          halign = "center";
-          valign = "center";
-        }
-      ];
     };
   };
 }
