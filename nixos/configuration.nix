c8b777dee41c6dce1a4403157285ac04e66cc14b
Trace Ohrt <teohrt18@gmail.com>
2026-04-05 19:05:17 -0400

    use binary caches isntead of rebuilding, also use parallel jobs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,7 +8,20 @@
     ./themes.nix
   ];
   
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    # Build using all available cores
+    max-jobs = "auto";
+    # Pull pre-built binaries from nix-community cache (home-manager, stylix, etc.)
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
