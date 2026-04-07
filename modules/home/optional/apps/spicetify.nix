6ef6dc45a49b96dcb925b978d80521797e62f14c
Trace Ohrt <teohrt18@gmail.com>
2026-04-07 11:59:17 -0400

    missing package

--- modules/home/optional/apps/spicetify.nix
+++ modules/home/optional/apps/spicetify.nix
@@ -1,8 +1,15 @@
-{ spicetify-nix, ... }:
+{ spicetify-nix, pkgs, ... }:
 
 {
   imports = [ spicetify-nix.homeManagerModules.default ];
 
-  programs.spicetify.enable = true;
+  programs.spicetify = {
+    enable = true;
+    # brotli is an undeclared runtime dep of newer Spotify versions;
+    # adding it here ensures spicetify-nix's wrapper includes it in LD_LIBRARY_PATH
+    spotifyPackage = pkgs.spotify.overrideAttrs (old: {
+      buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.brotli ];
+    });
+  };
   # stylix.targets.spicetify handles the theme automatically
 }
