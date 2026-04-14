f1e74139ccb40818bbe9b51717be74e8b16f5ac8
Trace Ohrt <teohrt18@gmail.com>
2026-04-13 23:31:17 -0400

    zsh and app organization

--- modules/nixos/optional/system-apps.nix
+++ modules/nixos/optional/system-apps.nix
@@ -3,12 +3,22 @@
   environment.systemPackages = with pkgs; [
     git
     neovim
-    eza # A modern replacement for ‘ls’
+    eza # A modern replacement for 'ls'
     alacritty
     vscode
     gnumake
     stow
     nerd-fonts.jetbrains-mono
     home-manager
+    satty
   ];
+
+  xdg.mime.defaultApplications = {
+    "image/png" = "satty.desktop";
+    "image/jpeg" = "satty.desktop";
+    "image/gif" = "satty.desktop";
+    "image/webp" = "satty.desktop";
+    "image/bmp" = "satty.desktop";
+    "image/tiff" = "satty.desktop";
+  };
 }
