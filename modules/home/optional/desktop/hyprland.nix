b02a08d06fbae5b65800e521f53cf7086a886c2c
Trace Ohrt <teohrt18@gmail.com>
2026-05-10 13:27:21 -0400

    hyprland border logic

--- modules/home/optional/desktop/hyprland.nix
+++ modules/home/optional/desktop/hyprland.nix
@@ -570,12 +570,19 @@ in
         "animation slide top, swaync-control-center"   # notification panel slides down from top
       ];
 
+      # remove borders when only one tiled window on a workspace
+      workspace = [
+        "w[tv1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
+      ];
+
       # floating window rules for TUI apps launched in titled windows
       windowrulev2 = [
+        "bordersize 0, floating:0, onworkspace:w[tv1]"
+        "bordersize 0, floating:0, onworkspace:f[1]"
 
         "workspace 1, class:^(chromium-browser|google-chrome|Chromium)$"
         "workspace 2, class:^(kitty)$, initialTitle:^(kitty)$"
-        "noborder,   class:^(kitty)$, initialTitle:^(kitty)$"
         "workspace 3, class:^(code|Code|code-url-handler)$"
         "workspace 6, class:^(Slack|slack)$"
         "workspace 7, class:^(obsidian)$"
