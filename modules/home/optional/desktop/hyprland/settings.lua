7f96016917fb8ecf074dd2b9a624833ada705887
Trace Ohrt <teohrt18@gmail.com>
2026-06-30 14:39:51 -0400

    wallpaper

--- modules/home/optional/desktop/hyprland/settings.lua
+++ modules/home/optional/desktop/hyprland/settings.lua
@@ -14,6 +14,9 @@ hl.config({
 
     misc = {
         focus_on_activate = true,
+        force_default_wallpaper = 0,
+        disable_hyprland_logo = true,
+        disable_splash_rendering = true,
     },
 
     input = {
