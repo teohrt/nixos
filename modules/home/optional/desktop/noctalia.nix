998968d1c2bb4bcbee5503adca1a5263cfce242d
Trace Ohrt <teohrt18@gmail.com>
2026-07-12 18:40:06 -0400

    bar

--- modules/home/optional/desktop/noctalia.nix
+++ modules/home/optional/desktop/noctalia.nix
@@ -14,7 +14,7 @@
       bar.main = {
         position = "bottom";
         scale = 1.3;
-        widget_spacing = 19;
+        widget_spacing = 32;
         radius = 0;
         margin_edge = 0;
         margin_ends = 0;
@@ -38,7 +38,7 @@
       control_center = {
         sidebar = "none";
         sidebar_section = "none";
-        calendar.show_events_card = false;
+        calendar.show_events_card = true;
       };
 
       theme = {
@@ -47,19 +47,61 @@
         builtin = "Nord";
       };
 
-      location.address = "New York, US";
+      location.address = "Brooklyn, NY";
 
       dock.enabled = false;
       desktop_widgets.enabled = true;
       lockscreen_widgets.enabled = false;
 
+      notification = {
+        show_actions = false;
+        show_app_name = false;
+      };
+
+      osd = {
+        position = "top_right";
+        kinds.media = false;
+      };
+
       shell = {
+        external_ip_enabled = true;
         ui_scale = 1.2;
-        animation.speed = 1.4;
+        animation.speed = 1.6;
+        screenshot.directory = "/home/trace/Pictures/Screenshots";
         panel = {
           control_center_placement = "attached";
           open_near_click_control_center = true;
         };
+        session.actions = [
+          {
+            action = "lock";
+            shortcut = "1";
+            enabled = true;
+            countdown_seconds = 0.0;
+            variant = "default";
+          }
+          {
+            action = "logout";
+            shortcut = "2";
+            enabled = true;
+            countdown_seconds = 0.0;
+            variant = "default";
+          }
+          {
+            action = "reboot";
+            shortcut = "3";
+            enabled = true;
+            countdown_seconds = 0.0;
+            variant = "default";
+          }
+          {
+            action = "shutdown";
+            shortcut = "4";
+            enabled = true;
+            countdown_seconds = 0.0;
+            variant = "destructive";
+          }
+        ];
       };
 
       widget = {
@@ -67,9 +109,14 @@
           format = "{:%-I:%M %p}";
           anchor = true;
         };
-        control-center.glyph = "parachute";
+        control-center.glyph = "snowflake";
         cpu.display = "text";
-        ram.display = "text";
+        notification-history.enabled = true;
+        ram = {
+          show_label = false;
+          display = "text";
+          stat = "ram_pct";
+        };
         temp.display = "text";
         network.show_label = false;
         volume.show_label = false;
